676aefca27c3a7f28eb50fbae0c874766e3c7e16
xw <xw188@yum.com>
2022-03-22 22:49:49 +0800

    2-7增加了hello接口测试test.http

--- src/main/java/com/yum/wiki/controller/TestController.java
+++ src/main/java/com/yum/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.yum.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,5 +14,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello world!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(@RequestParam String name) {
+        return "hello Post!" + name;
     }
 }
