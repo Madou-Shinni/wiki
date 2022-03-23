afa9b191087bae41eb59ad1b4bf6e02483c94dfc
xw <xw188@yum.com>
2022-03-23 11:41:54 +0800

    3-6 电子书代码生成

---
+++ src/main/java/com/yum/wiki/mapper/EbookMapper.java
@@ -0,0 +1,32 @@
+package com.yum.wiki.mapper;
+
+import com.yum.wiki.domain.Ebook;
+import com.yum.wiki.domain.EbookExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+
+}
