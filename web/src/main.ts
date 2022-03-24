8a8b599958b17f3f12af8d76213f5c7cff54d266
xw <xw188@yum.com>
2022-03-24 10:57:17 +0800

    5-6 配置axios拦截器打印请求参数和返回参数

--- web/src/main.ts
+++ web/src/main.ts
@@ -8,10 +8,30 @@ import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue'
 import axios from 'axios'
 
-axios.defaults.url = process.env.VUE_APP_SERVER
+axios.defaults.baseURL = process.env.VUE_APP_SERVER
+/**
+ * axios拦截器
+ */
+// http request 拦截器
+axios.interceptors.request.use(
+    config => {
+        console.log('请求参数：',config)
+        return config
+    },error => {
+        return Promise.reject(error)
+    })
+// http response 拦截器
+axios.interceptors.response.use(
+    response => {
+        console.log('返回结果：',response)
+        return response
+    },error => {
+        console.log('返回错误：',error)
+        return Promise.reject(error)
+    })
+
 
 //createApp(App).use(store).use(router).use(Antd).mount('#app')
-
 // 分离
 const app = createApp(App);
 app.use(store).use(router).use(Antd).mount('#app');
@@ -21,6 +41,3 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i,icons[i])
 }
-
-console.log('环境：',process.env.NODE_ENV)
-console.log('server地址：',process.env.VUE_APP_SERVER)
