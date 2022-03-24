5763ad7deaf0b3707b5305f2358d8022f7091042
xw <xw188@yum.com>
2022-03-24 14:24:50 +0800

    6-2 增加知识库管理页面 新页面开发：页面、路由、菜单

--- web/src/components/The-Header.vue
+++ web/src/components/The-Header.vue
@@ -6,9 +6,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="1">
+        <router-link to="/">首页</router-link>
+      </a-menu-item>
+      <a-menu-item key="2">
+        <router-link to="/admin/ebook">知识库</router-link>
+      </a-menu-item>
+      <a-menu-item key="3">
+        <router-link to="/about">关于我们</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
---
+++ web/src/views/admin/Admin-Ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>知识库管理</h1>
+  </div>
+</template>
