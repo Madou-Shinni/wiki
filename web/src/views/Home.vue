d5fcc69dbcb93f67dc99fb7593a0054d1d9af0a9
xw <xw188@yum.com>
2022-03-26 12:22:59 +0800

    7-7 首页显示分类菜单

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -5,41 +5,20 @@
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-              <span>
-                <user-outlined />
-                subnav 1
-              </span>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined/>{{ item.name }}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item :key="child.id" v-for="child in item.children">
+            <MailOutlined/>
+            <span>{{ child.name }}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -53,7 +32,7 @@
           <a-list-item key="item.name">
             <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
+            <component v-bind:is="type" style="margin-right: 8px"/>
             {{ text }}
           </span>
             </template>
@@ -63,7 +42,9 @@
                 <a :href="item.href">{{ item.name }}</a>
               </template>
               <!--封面-->
-              <template #avatar><a-avatar :src="item.cover" /></template>
+              <template #avatar>
+                <a-avatar :src="item.cover"/>
+              </template>
             </a-list-item-meta>
             {{ item.content }}
           </a-list-item>
@@ -74,8 +55,9 @@
 </template>
 
 <script lang="ts">
-import {defineComponent, onMounted, reactive, ref,toRef} from 'vue';
+import {defineComponent, onMounted, ref} from 'vue';
 import axios from "axios";
+import {message} from "ant-design-vue";
 
 export default defineComponent({
   name: 'Home',
@@ -83,17 +65,6 @@ export default defineComponent({
   setup() {
     // ref: 响应式数据(获取和赋值都需要.value)
     const ebooks = ref();
-    // reactive: 里面一般是放一个对象
-    const ebooks1 = reactive({books:[]});
-    onMounted(()=>{
-      axios.get("/ebook/list").then(
-          (response)=>{
-            const data = response.data
-            // 我们后端封装的返回数据的数据集是对象.data
-            ebooks.value = data.data;
-            ebooks1.books = data.data;
-          })
-    });
 
     // 列表数据分页
     const pagination = {
@@ -103,16 +74,49 @@ export default defineComponent({
       pageSize: 3,
     };
     const actions: Record<string, string>[] = [
-      { type: 'StarOutlined', text: '156' },
-      { type: 'LikeOutlined', text: '156' },
-      { type: 'MessageOutlined', text: '2' },
+      {type: 'StarOutlined', text: '156'},
+      {type: 'LikeOutlined', text: '156'},
+      {type: 'MessageOutlined', text: '2'},
     ];
+
+    /**
+     * 查询所有分类
+     */
+    const level1 = ref();
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data
+        if (data.success) {
+          level1.value = [];
+          level1.value = data.data;
+        } else {
+          message.error(data.message);
+        }
+      })
+    };
+
+    onMounted(() => {
+      handleQueryCategory();
+      axios.get("/ebook/list", {
+        params: {
+          page: 1,
+          size: 1000
+        }
+      }).then(
+          (response) => {
+            const data = response.data
+            // 我们后端封装的返回数据的数据集是对象.data
+            ebooks.value = data.data;
+          })
+    })
 
     // 返回数据让页面能够使用
     return {
       ebooks,
       pagination,
       actions,
+      /* 分类菜单 */
+      level1,
     }
   }
 });
