cf328e761acf57e6c4703447232df7a1cb866d78
xw <xw188@yum.com>
2022-03-29 17:50:23 +0800

    9-12 前端增加路由拦截，未登录时跳转到首页

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
+import {Tool} from "@/util/tool";
+import store from "@/store";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,22 +20,34 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/admin/user',
     name: '/Admin-User.vue',
-    component: () => import('../views/admin/Admin-User.vue')
+    component: () => import('../views/admin/Admin-User.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/ebook',
     name: '/Admin-Ebook.vue',
-    component: () => import('../views/admin/Admin-Ebook.vue')
+    component: () => import('../views/admin/Admin-Ebook.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/category',
     name: '/Admin-Category.vue',
-    component: () => import('../views/admin/Admin-Category.vue')
+    component: () => import('../views/admin/Admin-Category.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/admin/doc',
     name: '/Admin-Doc.vue',
-    component: () => import('../views/admin/Admin-Doc.vue')
+    component: () => import('../views/admin/Admin-Doc.vue'),
+    meta: {
+      loginRequire: true
+    }
   },
   {
     path: '/doc',
@@ -46,5 +60,24 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+// 路由登录拦截
+router.beforeEach((to, from, next) => {
+  // 要不要对meta.loginRequire属性做监控拦截
+  if (to.matched.some(function (item) {
+    //console.log(item, "是否需要登录校验：", item.meta.loginRequire);
+    return item.meta.loginRequire
+  })) {
+    const loginUser = store.state.user;
+    if (Tool.isEmpty(loginUser)) {
+      //console.log("用户未登录！");
+      next('/');
+    } else {
+      next();
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
