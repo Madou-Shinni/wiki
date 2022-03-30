9b9c034be1adc8f2c1123e262bccd8707934d2de
xw <xw188@yum.com>
2022-03-30 15:00:47 +0800

    10-10 加上事务注解

--- src/main/java/com/yum/wiki/service/DocService.java
+++ src/main/java/com/yum/wiki/service/DocService.java
@@ -26,6 +26,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ObjectUtils;
 
 import java.util.List;
@@ -161,6 +162,7 @@ public class DocService {
     /**
      * 新增文档
      */
+    @Transactional
     public void save(DocSaveReq req) {
         Doc doc = CopyUtil.copy(req,Doc.class);
         Content content = CopyUtil.copy(req,Content.class);
