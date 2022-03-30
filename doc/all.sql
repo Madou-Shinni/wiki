de2378d5bd6e323404649da97b7789ec53b68cc6
xw <xw188@yum.com>
2022-03-30 18:27:36 +0800

    11-4 知识库快照表增加唯一键，一个知识库一天只能有一条记录

--- doc/all.sql
+++ doc/all.sql
@@ -110,5 +110,42 @@ create table `ebook_snapshot` (
 `vote_count` int not null default 0 comment '点赞数' ,
 `view_increase` int not null default 0 comment '阅读增长',
 `vote_increase` int not null default 0 comment '点赞增长' ,
-primary key (`id`)
+primary key (`id`),
+unique key `ebook_id_date_unique` (`ebook_id`,`date`)
 ) engine=innodb default charset=utf8mb4 comment= '知识库快照表';
+
+# 方案一(ID不连续) :
+#   删除今天的数据
+#   为所有的知识库生成- -条今天的记录
+#   更新总阅读数、总点赞数
+#   更新今日阅读数、 今日点赞数
+
+
+# 方案二(ID连续):
+#   为所有的知识库生成一条今天的记录，如果还没有
+#   更新总阅读数、总点赞数
+#   更新今日阅读数、今日点赞数
+insert into ebook_snapshot(ebook_id, `date`, view_count, vote_count, view_increase, vote_increase)
+select t1.id, curdate(), 0, 0, 0, 0
+from ebook t1
+where not exists(select 1
+                 from ebook_snapshot t2
+                 where t1.id = t2.ebook_id
+                   and t2.`date` = curdate());
+
+update ebook_snapshot t1, ebook t2
+set t1.view_count = t2.view_count,t1.vote_count = t2.view_count
+where t1.`date` = curdate() and t1.ebook_id = t2.id;
+
+# 获取昨天的数据
+select t1.ebook_id,view_count,vote_count from ebook_snapshot t1
+where t1.`date` = date_sub(curdate(),interval 1 day );
+
+update ebook_snapshot t1 left join
+    (select ebook_id,view_count,vote_count from ebook_snapshot
+     where `date` = date_sub(curdate(),interval 1 day )) t2
+    on t1.ebook_id = t2.ebook_id
+set t1.view_increase = (t1.view_count - ifnull(t2.view_count,0)),
+    t1.vote_increase = (t1.vote_count - ifnull(t2.vote_count,0))
+where t1.`date` = curdate();
+# ---------------------------------------
